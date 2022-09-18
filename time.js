f0bb0b813dca4f3dce38843928b83cfc81591076
Edneusa <edneusarosario@gmail.com>
2022-09-18 14:10:44 -0300

    time de futebol

---
+++ time.js
@@ -0,0 +1,44 @@
+let time = [
+    {
+        nome:"Orlando",
+        camisa: 10,
+
+    },
+    {
+        nome:"Marcelo",
+        camisa: 9,
+    },{
+        nome:"Neymar",
+        camisa: 11,
+    },{
+        nome:"Edmilson",
+        camisa:20,
+    },{
+        nome:"Gabriel",
+        camisa: 30,
+    },{
+        nome:"Cristovan",
+        camisa: 1,
+    },
+    {
+        nome:"Vitor",
+        camisa: 5,
+    },{
+        nome:"Hugo",
+        camisa: 15,
+    },{
+        nome:"Pedro",
+        camisa: 18,
+    },{
+        nome:"Mario",
+        camisa: 03,
+    },{
+        nome:"Alex",
+        camisa: 8,
+    },{
+        nome:"Andre",
+        camisa: 7,
+    },
+]
+    console.log(time.length)
+    console.table(time)
