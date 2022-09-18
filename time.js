e85dcac99beb199964a3aa4411deea9ac2b77cda
Edneusa <edneusarosario@gmail.com>
2022-09-18 14:42:39 -0300

    time de Futebol

--- time.js
+++ time.js
@@ -42,3 +42,17 @@ let time = [
 ]
     console.log(time.length)
     console.table(time)
+
+    //for (let i = 0; < time. length: i++) {
+        // console. log(i, time[i]);
+    //}
+
+    for (const i in time){
+        console.log(i,time[i]);
+
+    }
+    for (const jogador of time) {
+        console.log(jogador);        
+    }
+
+    console.table(time);
